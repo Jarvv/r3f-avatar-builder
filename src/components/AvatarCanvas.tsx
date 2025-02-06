bb322aaef8985d284b6c06fada9a6822b29d5d7e
James Harvey <james.harvey97@live.com>
2025-02-06 14:40:19 +0000

    feat: postprocessing

--- src/components/AvatarCanvas.tsx
+++ src/components/AvatarCanvas.tsx
@@ -1,14 +1,16 @@
 import { Environment, SoftShadows } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Bloom, EffectComposer, N8AO } from "@react-three/postprocessing";
 import { Avatar } from "@/components/Avatar";
 import { CameraManager } from "@/components/CameraManager";
 import { Lights } from "@/components/Lights";
+import { CAMERA } from "@/const";
 
 export const AvatarCanvas = () => {
   return (
     <Canvas
       shadows
-      camera={{ position: [-1, 3, 5], fov: 45 }}
+      camera={{ position: CAMERA.POSITION, fov: 45 }}
       gl={{
         preserveDrawingBuffer: true,
       }}
@@ -24,6 +26,17 @@ export const AvatarCanvas = () => {
       <Lights />
       <SoftShadows size={52} samples={16} focus={0.5} />
       <Avatar />
+      <EffectComposer>
+        <Bloom mipmapBlur luminanceThreshold={1.2} intensity={1.2} />
+        <N8AO
+          quality="performance"
+          halfRes
+          aoRadius={50}
+          distanceFalloff={0.2}
+          intensity={6}
+          screenSpaceRadius
+        />
+      </EffectComposer>
     </Canvas>
   );
 };
